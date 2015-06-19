bf6bb6cfa6fe364f0ff23e325031c242b6b44a86
Dave Reid <dave@davereid.net>
2015-06-19 16:19:45 -0500

    Support context substitution in SimpleLogger

--- src/SimpleLogger.php
+++ src/SimpleLogger.php
@@ -21,6 +21,16 @@ class SimpleLogger implements LoggerInterface
 
     public function log($level, $message, array $context = array())
     {
+        // The message MAY contain placeholders in the form: {foo} where foo
+        // will be replaced by the context data in key "foo".
+        if (!empty($context)) {
+            $replace = array();
+            foreach ($context as $key => $val) {
+              $replace['{' . $key . '}'] = $val;
+            }
+            $message = strtr($message, $replace);
+        }
+
         if (is_resource($this->writeTo)) {
             fwrite($this->writeTo, "[{$level}] {$message}\n");
         } elseif (is_callable($this->writeTo)) {
